fd63af57fdce7505b07c863341083713ac453453
Tabithadejong <tabithadejong654@gmail.com>
2023-01-19 12:14:46 +0000

    Add context to the Modal

--- assets /js /script.js
+++ assets /js /script.js
@@ -1,5 +1,3 @@
-alert("i am connected")
-
 const API_KEY = "Uey9_jWswJqeZywhUG5vBqukYr8"; 
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
@@ -15,10 +13,21 @@ async function getStatus(e) {
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data.expiry);
+        displayStatus(data);
+    } else { 
+        throw new Error(data.error)
     }
 
+
 }
+
+function displayStatus(data) { 
+    document.getElementById('resultsModalTitle').innerText = "API Key Status"; 
+    document.getElementById('results-content').innerHTML = `Your key is valid untill: \n ${data.expiry}`
+    resultsModal.show(); 
+}
+
+
 
 
 
